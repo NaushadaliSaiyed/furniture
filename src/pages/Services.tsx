bc1bfeb216dff58d432e2890ce60d7018848c85d
Whitestrom2040 <mo.sharifmamani91@gmail.com>
2023-05-11 17:01:00 +0530

    sharif change
    DVZSdzsv

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import Book from '../components/animation/Book'
+import { FaBasketballBall } from 'react-icons/fa';
+import SarviceProduct from '../components/service/SarviceProduct';
 
 const Services = () => {
   return (
-    <div>
-      sharif
-    </div>
+    <>
+      <div className='bg-white h-[63%] w-[84%] mt-28 flex'>
+        <div>
+          <h1 className='mt-20 ml-20 font-bold text-5xl'> Software Development <br />Services</h1>
+          <p className='mt-12 ml-20 text-xl'> We love to shake established thought-patterns and create what has never <br /> been attempted before. we take seeds and grow jungles </p>
+          <div className='flex gap-3 items-center ml-20 mt-24'>
+            <p className='font-bold font-serif'> M Be </p>
+            <FaBasketballBall />
+            <p className='font-bold from-neutral-900'>C </p>
+          </div>
+        </div>
+        <div>
+          <Book />
+        </div>
+      </div>
+      <SarviceProduct/>
+    </>
   )
 }
 
