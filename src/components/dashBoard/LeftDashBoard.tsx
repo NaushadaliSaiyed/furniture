9ee92043acdfb8dc818d0d694d3d92d17c6b9b77
NaushadaliSaiyed <127712400+NaushadaliSaiyed@users.noreply.github.com>
2023-05-11 15:39:41 +0530

    naushad Chaning

--- src/components/dashBoard/LeftDashBoard.tsx
+++ src/components/dashBoard/LeftDashBoard.tsx
@@ -3,17 +3,15 @@ import React from 'react'
 const LeftDashBoard = () => {
   return (
     <div className='w-[30%] h-full bg-[#3E3D45]'>
-      {/* <div className='fixed]'>
-        <h1 className='text-[50vh] text-[#48474D]'>01</h1>
-      </div> */}
-      <div className='flex flex-col h-[100%] text-white bg-red-300'>
-        <p className='z-10 '>INTERIOR</p>
-        {/* text-[150px] relative ml-[3rem] font-bold text-[#70705b] [text-shadow:_5px_5px_4px_#bfc0bd] font-serif */}
-        <p className='z-10'>Design</p>
-        {/* w-[17%] ml-28 */}
-        <p className='z-10'>The stylish interior makes us want to spend more time in it. We present interesrting sets to create an apartment. We choose the right colors according to our own taste.</p>
-        {/* ml-28 */}
-        <p className='z-10'>Read more</p>
+      {/* fixed w-full h-full justify-center items-center */}
+      <div className='divkadiv'>
+        <h1 className='text-white'>01</h1>
+      </div>
+      <div className='flex flex-col h-[100%] text-white font-mono justify-end p-[10%] gap-3 relative top-0 '>
+        <p className='z-10 font-semibold text-[#bfc0bd]'>INTERIOR</p>
+        <p className='z-10 text-[96px] font-bold text-[#ffffff] [text-shadow:_5px_5px_4px_#bfc0bd] font-serif'>Design</p>
+        <p className='z-10 font-semibold text-[#bfc0bd] mb-10'>The stylish interior makes us want to spend more time in it. We present interesrting sets to create an apartment. We choose the right colors according to our own taste.</p>
+        <p className='z-10 font-semibold text-[#bfc0bd]'>Read more</p>
       </div>
     </div>
   )
