30040078caa340281e29fa6ab06e54913e77d822
nehadesai1392000 <nehadesai1392000@gmail.com>
2023-05-11 17:46:15 +0530

    okkkk

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -4,21 +4,106 @@ const Product = () => {
   return (
 
     <div className='h-screen w-full'>
-      <div className=' h-[680px] flex flex-col ml-8 gap-16'>
+      <div className=' h-[680px] flex flex-col ml-28 gap-16'>
         <p className='font-semibold text-5xl mt-64'>Case Studies</p>
         <p className='w-[890px] text-1xl '>We believe that great products cannot happen without passion, intelligence, and personal commitment, and it is demonstrated by our portfolio that spans different customers, industries, and target audiences.</p>
       </div>
-      <div className='bg-slate-400 h-[680px] flex  '>
-        <div className='mt-36'>
-          <img src="../src/assets/symbioosa-lighting-llev.jpg" alt="" className='w-[370px] h-[450px]' />
-          <p>Product Design</p>
-          <p>Issue tracking app</p>
-          <p>Make your work routine simple and convenient, say goodbye to the stress and chaos of managing multiple tasks and hello to a more productive workday</p>
+      <div className='flex gap-24'>
+        <div className='mt-36 ml-28  '>
+          <img src="../src/assets/symbioosa-lighting-llev.jpg" alt="" className='w-[530px] h-[480px]' />
+          <p className='font-semibold mt-7'>Product Design</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>Issue tracking app</p>
+          <p className='font-semibold mt-7 ' >Make your work routine simple and convenient, <br /> say goodbye to the stress and chaos of managing multiple <br /> tasks and hello to a more productive workday</p>
         </div>
-        <div className='mt-48'>
-           <img src="../src/assets/bob-corner-sofa-module.jpg" alt="" className='w-[370px] h-[450px]' />
+        <div className='mt-48 mr-28'>
+           <img src="../src/assets/bob-corner-sofa-module.jpg" alt="" className='w-[570px] h-[450px]' />
+           <p className='font-semibold mt-7'>UX/UI Design Research</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>Homely app</p>
+          <p className='font-semibold mt-7 ' >The main goal of the platform is to offer high-level,<br /> elegant, and modern properties across Europe and South America.</p>
         </div>
       </div>
+
+      <div className='flex gap-24'>
+        <div className='mt-36 ml-28  '>
+          <img src="../src/assets/istockphoto2.jpg" alt="" className='w-[530px] h-[520px]' />
+          <p className='font-semibold mt-7'>UX/UI Design Research</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>The #1 app for making <br /> music on your phone</p>
+          <p className='font-semibold mt-7 ' >Join 10 million other music creators</p>
+        </div>
+        <div className='mt-52 mr-28'>
+           <img src="../src/assets/istockphoto3.jpg" alt="" className='w-[570px] h-[450px]' />
+           <p className='font-semibold mt-7'>Product Design</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>Duds app</p>
+          <p className='font-semibold mt-7 ' >Shazam for clothes. An antidote to the feeling when you <br/>  really
+           want some sneakers that you saw on a blogger,  but he doesn't respond <br /> to comments, and you can't find them on the brand's website</p>
+        </div>
+      </div>
+
+
+
+      <div className='flex gap-24'>
+        <div className='mt-36 ml-28  '>
+          <img src="../src/assets/istockphoto.jpg" alt="" className='w-[530px] h-[520px]' />
+          <p className='font-semibold mt-7'>Product Design</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>Issue tracking app</p>
+          <p className='font-semibold mt-7 ' >Make your work routine simple and convenient, <br /> say goodbye to the stress and chaos of managing multiple <br /> tasks and hello to a more productive workday</p>
+        </div>
+        <div className='mt-52 mr-28'>
+           <img src="../src/assets/istockphoto1.jpg" alt="" className='w-[570px] h-[450px]' />
+           <p className='font-semibold mt-7'>UX/UI Design Research</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>Homely app</p>
+          <p className='font-semibold mt-7 ' >The main goal of the platform is to offer high-level,<br /> elegant, and modern properties across Europe and South America.</p>
+        </div>
+      </div>
+
+      <div className='flex gap-24'>
+        <div className='mt-36 ml-28  '>
+          <img src="../src/assets/istockphoto4.jpg" alt="" className='w-[530px] h-[520px]' />
+          <p className='font-semibold mt-7'>Product Design</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>Issue tracking app</p>
+          <p className='font-semibold mt-7 ' >Make your work routine simple and convenient, <br /> say goodbye to the stress and chaos of managing multiple <br /> tasks and hello to a more productive workday</p>
+        </div>
+        <div className='mt-52 mr-28'>
+           <img src="../src/assets/bob-corner-sofa-module5.jpg" alt="" className='w-[570px] h-[450px]' />
+           <p className='font-semibold mt-7'>UX/UI Design Research</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>Homely app</p>
+          <p className='font-semibold mt-7 ' >The main goal of the platform is to offer high-level,<br /> elegant, and modern properties across Europe and South America.</p>
+        </div>
+      </div>
+
+      <div className='flex gap-24'>
+        <div className='mt-36 ml-28  '>
+          <img src="../src/assets/istockphoto6.jpg" alt="" className='w-[530px] h-[520px]' />
+          <p className='font-semibold mt-7'>Product Design</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>Issue tracking app</p>
+          <p className='font-semibold mt-7 ' >Make your work routine simple and convenient, <br /> say goodbye to the stress and chaos of managing multiple <br /> tasks and hello to a more productive workday</p>
+        </div>
+        <div className='mt-52 mr-28'>
+           <img src="../src/assets/istockphoto7.jpg" alt="" className='w-[570px] h-[450px]' />
+           <p className='font-semibold mt-7'>UX/UI Design Research</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>Homely app</p>
+          <p className='font-semibold mt-7 ' >The main goal of the platform is to offer high-level,<br /> elegant, and modern properties across Europe and South America.</p>
+        </div>
+      </div>
+
+      <div className='flex gap-24'>
+        <div className='mt-36 ml-28  '>
+          <img src="../src/assets/istockphoto8.jpg" alt="" className='w-[530px] h-[520px]' />
+          <p className='font-semibold mt-7'>Product Design</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>Issue tracking app</p>
+          <p className='font-semibold mt-7 ' >Make your work routine simple and convenient, <br /> say goodbye to the stress and chaos of managing multiple <br /> tasks and hello to a more productive workday</p>
+        </div>
+        <div className='mt-52 mr-28'>
+           <img src="../src/assets/istockphoto9.jpg" alt="" className='w-[570px] h-[450px]' />
+           <p className='font-semibold mt-7'>UX/UI Design Research</p>
+          <p className='font-semibold text-4xl mt-5 hover:text-orange-600'>Homely app</p>
+          <p className='font-semibold mt-7 ' >The main goal of the platform is to offer high-level,<br /> elegant, and modern properties across Europe and South America.</p>
+        </div>
+      </div>
+
+
+
+
 
     </div>
 
