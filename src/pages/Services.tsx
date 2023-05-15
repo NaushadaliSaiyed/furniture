9dcd21e50e21a1ab65c7e6855ff10f55d4f5f542
Whitestrom2040 <mo.sharifmamani91@gmail.com>
2023-05-15 09:31:05 +0530

    sharif  changies
    svzsd

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -11,9 +11,9 @@ const Services = () => {
           <p className='mt-28 ml-20 font-bold text-5xl'>Affordable Luxury <br /> <br />  Discover Your Dream <span className='text-orange-600'>Home</span> </p>
           <p className='mt-12 ml-20 text-xl'> We love to shake established thought-patterns and create what has never <br /> been attempted before. we take seeds and grow jungles </p>
           <div className='flex gap-3 items-center ml-20 mt-52'>
-            <p className='font-bold font-serif'> M Be </p>
+            <p className='font-bold font-serif text-xl'> M Be </p>
             <FaBasketballBall />
-            <p className='font-bold from-neutral-900'>C </p>
+            <p className='font-bold from-neutral-900 text-xl'>C </p>
           </div>
         </div>
         <div className='ml-32'>
