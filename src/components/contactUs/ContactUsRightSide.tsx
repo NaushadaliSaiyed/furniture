242fe6990c95bd34c47c07bb5588add1921c162b
Whitestrom2040 <mo.sharifmamani91@gmail.com>
2023-05-15 14:41:22 +0530

    dszscv
    dzvdvs

--- src/components/contactUs/ContactUsRightSide.tsx
+++ src/components/contactUs/ContactUsRightSide.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { AiOutlineCopy } from 'react-icons/ai'
+
 
 const ContactUsRightSide = () => {
     return (
         <div className='w-[100%]'>
-            email
-            {/* <div>Email</div> */}
-            {/* <div></div>
-            <div></div> */}
+            <p className='text-2xl font-semibold'>Email</p>
+            <span className='flex mt-3 text-center items-center gap-2'>
+                <p className='text-orange-400 cursor-pointer'>rohit@tapatap.com</p>
+                <AiOutlineCopy className='text-lg' />
+            </span>
+            <p className='text-2xl font-semibold mt-12'>Phones</p>
+            <div className='flex justify-between'>
+                <span>
+                    <p className='mt-2'>Russia</p>
+                    <p> +07 (65) 785 9874</p>
+                </span>
+                <span>
+                    <p className='mt-2'>India</p>
+                    <p> +91 (98) 5401 2040</p>
+                </span>
+                <span>
+                    <p className='mt-2'>France</p>
+                    <p> +33 (75) 786 6548</p>
+                </span>
+            </div>
+            <div></div>
         </div>
     )
 }
