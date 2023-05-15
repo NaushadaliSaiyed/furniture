b76bc4009a7a00308ad33f27af104e5cf48bdaaf
nehadesai1392000 <nehadesai1392000@gmail.com>
2023-05-15 12:18:22 +0530

    neha git

--- src/components/service/SarviceProduct.tsx
+++ src/components/service/SarviceProduct.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { TbSofa } from 'react-icons/tb';
 import { GiBed } from 'react-icons/gi';
 import { GiWoodenChair } from 'react-icons/gi';
+import { FaFirstOrderAlt } from 'react-icons/fa';
+import { FaPaintRoller } from 'react-icons/fa';
+import { RiHomeGearFill } from 'react-icons/ri';
+import { BsGift } from 'react-icons/bs';
+
+
+
+
 
 
 // import Lottie from "lottie-react";
@@ -12,7 +20,7 @@ const SarviceProduct = () => {
     return (
         <div className='bg-neutral-100 '>
             <div>
-                <p className=' ml-28 font-semibold text-4xl mb-20'>Your One-Stop Shop for Trendy and Timeless Furniture</p>
+                <p className=' ml-28 font-semibold text-4xl mb-20 pt-10'>Your One-Stop Shop for Trendy and Timeless Furniture</p>
             </div>
             <div className='flex items-center ml-28 gap-24'>
                 <div className='bg-white h-[26rem] w-[21rem] flex flex-col items-center gap-5 p-4'>
@@ -37,8 +45,42 @@ const SarviceProduct = () => {
                         Wake Up 
                     </p>
                 </div>
-
             </div>
+
+
+            <div className='flex items-center ml-28 gap-24 mt-16'>
+                <div className='bg-white h-[26rem] w-[21rem] flex flex-col items-center gap-5 p-4'>
+                    <span><FaFirstOrderAlt className='text-8xl' /></span>
+                    <p className='font-medium text-3xl mt-5 hover:text-orange-600' >The art of furniture </p>
+                    <p className='w-52'> It can take on many shapes and styles, from sleek and modern to ornate and classic. The art of furniture involves a combination of design, craftsmanship, and functionality.
+                    </p>
+
+                </div>
+                <div className='bg-white h-[26rem] w-[21rem] flex flex-col items-center gap-5 p-6 '>
+                    <span><FaPaintRoller className='text-8xl  ' /></span>
+                    <p className='font-medium text-3xl mt-5 hover:text-orange-600'>Color when choosing </p>
+                    <p className='w-52'> Different colors can also evoke different emotions, with warm tones such as red and orange creating a cozy and inviting atmosphere, and cool tones such as blue and relaxing environment.</p>
+                </div>
+                <div className='bg-white h-[26rem] w-[21rem] flex flex-col items-center gap-5 p-6'>
+                    <span><RiHomeGearFill className='text-8xl' /></span>
+                    <p className='font-medium text-3xl mt-5 hover:text-orange-600'> Our Dream Home </p>
+                    <p  className='w-52'> From sofas and beds to tables and chairs, each piece of furniture serves a unique purpose. The right furniture can maximize space, improve organization,  home.
+                    </p>
+                </div>
+            </div>
+
+
+            <div className='flex items-center ml-28 gap-24 mt-16 pb-10'>
+                <div className='bg-white h-[26rem] w-[21rem] flex flex-col items-center gap-5 p-4'>
+                    <span><BsGift className='text-8xl' /></span>
+                    <p className='font-medium text-3xl mt-5 hover:text-orange-600' >Gift for a loved one </p>
+                    <p className='w-52'>A unique piece of furniture can add character to their space and become a cherished and practical item that they use daily. It's a gift that can truly last a lifetime.
+                    </p>
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
