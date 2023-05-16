41cef0961184ceb9cec63b799c17c1000df2e2f6
Whitestrom2040 <mo.sharifmamani91@gmail.com>
2023-05-16 14:36:53 +0530

    sharif changies
    dzfbzdf

--- src/components/contactUs/ContactLastDetails.tsx
+++ src/components/contactUs/ContactLastDetails.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Collapse } from 'antd';
+import '../../style/contactLastDetails.css'
+
+
+const { Panel } = Collapse;
 
 const ContactLastDetails = () => {
+
     return (
         <>
             <div className=''>
                 <p className='font-semibold text-lg'> Visit us </p>
-                <div className='mt-4 text-gray-500'>
+                <div className='mt-4 text-gray-500 cursor-pointer'>
                     Alaska <br />
                     ui. Massachusetts 25, 05-958 <br />
                     sharif@front.studio
@@ -39,7 +45,66 @@ const ContactLastDetails = () => {
                     <p>Contact Us</p>
                 </div>
             </span>
-            <div> <p className='font-semibold text-lg'>Details</p> </div>
+            <div>
+                <p className='font-semibold text-lg'>Details</p>
+                <div className='mt-4'>
+                    <Collapse
+                        style={{cursor:"pointer", fontWeight:"600"}}
+                        className='contact-last-details'
+                        bordered={false}
+                        defaultActiveKey={['0']}
+                    >
+                        <Panel header="Service" key="1">
+                            {"Agile Team of JS Programmers"} <br />
+                            {"Refreshing existing products"} <br />
+                            {"Building product from scratch"} <br />
+                            {"Digital Product Design"} <br />
+                            {"Branding"} <br />
+                            {"Motion Design"} <br />
+                        </Panel>
+                        <Panel header="Technologies" key="2">
+                            {"Agile Team of JS Programmers"} <br />
+                            {"Refreshing existing products"} <br />
+                            {"Building product from scratch"} <br />
+                            {"Digital Product Design"} <br />
+                            {"Branding"} <br />
+                            {"Motion Design"} <br />
+                        </Panel>
+                        <Panel header="Expertise" key="3">
+                            {"Agile Team of JS Programmers"} <br />
+                            {"Refreshing existing products"} <br />
+                            {"Building product from scratch"} <br />
+                            {"Digital Product Design"} <br />
+                            {"Branding"} <br />
+                            {"Motion Design"} <br />
+                        </Panel>
+                        <Panel header="Developers" key="4">
+                            {"Agile Team of JS Programmers"} <br />
+                            {"Refreshing existing products"} <br />
+                            {"Building product from scratch"} <br />
+                            {"Digital Product Design"} <br />
+                            {"Branding"} <br />
+                            {"Motion Design"} <br />
+                        </Panel>
+                        <Panel header="Industries" key="5">
+                            {"Agile Team of JS Programmers"} <br />
+                            {"Refreshing existing products"} <br />
+                            {"Building product from scratch"} <br />
+                            {"Digital Product Design"} <br />
+                            {"Branding"} <br />
+                            {"Motion Design"} <br />
+                        </Panel>
+                        <Panel header="Cities we operate in" key="6">
+                            {"Agile Team of JS Programmers"} <br />
+                            {"Refreshing existing products"} <br />
+                            {"Building product from scratch"} <br />
+                            {"Digital Product Design"} <br />
+                            {"Branding"} <br />
+                            {"Motion Design"} <br />
+                        </Panel>
+                    </Collapse>
+                </div>
+            </div>
             <span>
                 <p className='font-semibold text-lg'>Follow us</p>
                 <div className='mt-4 cursor-pointer text-gray-500'>
--- src/pages/ContactUS.tsx
+++ src/pages/ContactUS.tsx
@@ -12,14 +12,14 @@ const ContactUS = () => {
         <ContactUsLeftSide />
         <ContactUsRightSide />
       </div>
-      <div className='iframe'>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29376.2214223758!2d72.52587845!3d23.022755949999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1684144486769!5m2!1sen!2sin" width="100%"
+      <div className='iframe ml-28 '>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29376.2214223758!2d72.52587845!3d23.022755949999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1684144486769!5m2!1sen!2sin" width="91%"
           height="450"
           loading="lazy"
         >
         </iframe>
       </div>
-      <div className='pb-11 flex pt-14 pl-40 justify-between w-[90%]'>
+      <div className='flex pt-14 pl-32 justify-between w-[85%]'>
         <ContactLastDetails/>
       </div>
     </>
