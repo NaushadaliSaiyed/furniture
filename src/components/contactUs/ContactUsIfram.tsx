da5cf49e2968dd80c9a028f0edb905c184c46ef9
Whitestrom2040 <mo.sharifmamani91@gmail.com>
2023-05-18 15:24:15 +0530

    svdzv
    dzsvs

--- src/components/contactUs/ContactUsIfram.tsx
+++ src/components/contactUs/ContactUsIfram.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContactUsIfram = () => {
     return (
-        <div className='iframe ml-[8%] mt-[800px] md:mt-0'>
+        <div className='iframe my-7 ml-[8%] mt-[800px] md:mt-0'>
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29376.2214223758!2d72.52587845!3d23.022755949999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1684144486769!5m2!1sen!2sin" width="91.3%"
                 height="450"
                 loading="lazy"
