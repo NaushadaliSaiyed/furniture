e478db0642e38b68893d97ea89a7e66a2b6d86ef
Whitestrom2040 <mo.sharifmamani91@gmail.com>
2023-05-18 17:31:37 +0530

    fbdfb
    dfdfdfbfzf

--- src/components/hiring/Hiring.tsx
+++ src/components/hiring/Hiring.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import officepic from '../../assets/Image/laptopinoffice.jpg'
-import neha from '../../assets/Image/projectdiscuse.jpg'
+import neha from '../../assets/Image/nehaimage.jpeg'
+import sharif from "../../assets/Image/myphotoup.jpeg";
+import naushad from "../../assets/Image/naushadpic.jpg";
 
 
 const Hiring = () => {
     return (
-        <div className='flex justify-between mt-[130px] bg-slate-300 h-[450px] ml-[109px] w-[84%]'>
+        <>
+        <div className='flex justify-between mt-[130px] h-[450px] ml-[109px] w-[84%] border-b-2'>
             <div className='pt-32'>
                 <h1 className='text-3xl font-semibold font-sans'>Careers at Firefox</h1>
                 <p className='pt-14'> Welcome to the Firefox job portal! Join as to become a part of one of the <br />
                     most great IT companies in your career. A team with a clear vision, strong <br />
                     work ethic, and a lot of passion to the work that we do every day.</p>
             </div>
-                <img src={neha} alt="" className='h-[100px] w-[100px] rounded-full' />
+                <img src={neha} alt="" className='relative left-56 top-16 h-[150px] w-[150px] rounded-full' />
+                <img src={sharif} alt="" className='relative top-52 left-6 h-[85px] w-[85px] rounded-full' />
+                <img src={naushad} alt="" className='relative top-72 left-8 h-[95px] w-[95px] rounded-full' />
             <div>
                 <img src={officepic} alt="" className='h-[450px] w-[300px] rounded-tl-full rounded-bl-full' />
             </div>
         </div>
+            {/* <div className='border-b-2'></div> */}
+        </>
     )
 }
 
