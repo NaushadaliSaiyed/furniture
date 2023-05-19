208492e9e18814c689bdd2268dc5d01ea5daa2c2
Whitestrom2040 <mo.sharifmamani91@gmail.com>
2023-05-19 09:46:54 +0530

    sharif
    bbxdfbxdf

--- src/components/hiring/Hiring.tsx
+++ src/components/hiring/Hiring.tsx
@@ -8,21 +8,20 @@ import naushad from "../../assets/Image/naushadpic.jpg";
 const Hiring = () => {
     return (
         <>
-        <div className='flex justify-between mt-[130px] h-[450px] ml-[109px] w-[84%] border-b-2'>
-            <div className='pt-32'>
-                <h1 className='text-3xl font-semibold font-sans'>Careers at Firefox</h1>
-                <p className='pt-14'> Welcome to the Firefox job portal! Join as to become a part of one of the <br />
-                    most great IT companies in your career. A team with a clear vision, strong <br />
-                    work ethic, and a lot of passion to the work that we do every day.</p>
-            </div>
+            <div className='flex justify-between mt-[130px] h-[450px] ml-[109px] w-[84%] border-b-2'>
+                <div className='pt-32'>
+                    <h1 className='text-3xl font-semibold font-sans'>Careers at Firefox</h1>
+                    <p className='pt-14'> Welcome to the Firefox job portal! Join as to become a part of one of the <br />
+                        most great IT companies in your career. A team with a clear vision, strong <br />
+                        work ethic, and a lot of passion to the work that we do every day.</p>
+                </div>
                 <img src={neha} alt="" className='relative left-56 top-16 h-[150px] w-[150px] rounded-full' />
-                <img src={sharif} alt="" className='relative top-52 left-6 h-[85px] w-[85px] rounded-full' />
+                <img src={sharif} alt="" className='relative top-52 left-10 h-[80px] w-[80px] rounded-full' />
                 <img src={naushad} alt="" className='relative top-72 left-8 h-[100px] w-[100px] rounded-full' />
-            <div>
-                <img src={officepic} alt="" className='h-[450px] w-[300px] rounded-tl-full rounded-bl-full' />
+                <div>
+                    <img src={officepic} alt="" className='h-[450px] w-[300px] rounded-tl-full rounded-bl-full' />
+                </div>
             </div>
-        </div>
-            {/* <div className='border-b-2'></div> */}
         </>
     )
 }
